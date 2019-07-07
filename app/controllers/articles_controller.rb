f6a3151770486338d19c6c3d2b48f50e3636b237
monica-desjarlais <monicalauradesjarlais@gmail.com>
2019-07-07 15:42:26 +0300

    Fix create. flash and error messages,create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,27 @@
 class ArticlesController <ApplicationController
  def new
-   @article =Article.new
- end
+   @article =Article.new  #initiate the article variable, which will create a form at the UI level
+ end                      #Article = class name(model) - we create an object ( article) for this class
 
  def create
-   #render plain: params[:article].inspect
-@article=Article.new(article_params)
-@article.save
-redirect_to_articles_show(@article)
+ @article=Article.new(article_params) #To whitelist the values of article, we must create the method article_params
+   if @article.save
+   flash[:notice]= "Article was successfully created"  #notice is the name of message; will display in application
+   redirect_to article_path(@article)  #the index of the actions
+   else
+   render "new"    #if article is not saved, render the ‘new article’ template
+   end
 end
-#we create the method that we call above in Article.new
+
+def  show
+@article = Article.find(params[:id])
+end
+
+#we create the validating method that we call above in Article.new
 private
 def article_params
-#  :article = top-level key  which will permit the value of title and description
-params.require(:article).permit(:title, :description)
+# :article = top-level key  which will permit the value of title and description
+params.require(:article).permit(:title, :description) #require and permit are 2 predefined rails method
 end
 
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  # Here we define all the constraints for the atrributes title and description that will show in the error messages
 
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
   validates :description, presence: true, length: {minimum: 10, maximum: 300}
