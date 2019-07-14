d998d9c742f187ba59021a7e83aff70466b9e049
monica-desjarlais <monicalauradesjarlais@gmail.com>
2019-07-14 12:48:45 +0300

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+
+validates :username, presence: true,  #username is not empty,
+
+uniqueness: { case_sensitive: false }, #username is unique and ignores case sensitivity (eg: john -John)
+
+length: { minimum: 3, maximum: 25 }   # has this length,
+
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i   #valid email format, stored in a constant 
+
+validates :email, presence: true, length: { maximum: 105 },
+
+uniqueness: { case_sensitive: false },
+
+format: { with: VALID_EMAIL_REGEX }  #will check for valid email format
 end
