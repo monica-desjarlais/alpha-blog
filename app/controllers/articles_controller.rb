a5933840a21877b4847743928841cc6fa2a1b4fd
monica-desjarlais <monicalauradesjarlais@gmail.com>
2019-07-19 13:27:20 +0300

    Add admin user functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -63,7 +63,7 @@ end
 
 def require_same_user  #only user who created an article can edit/delete it
 
-if current_user != @article.user
+if current_user != @article.user and !current_user.admin?
 
 flash[:danger] = "You can only edit or delete your own articles"
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy] #restrict the delete users action to admin
 
   def new
     @user = User.new  # new instance of the class User
@@ -10,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save  #if user is saved (acomplishes all conditions)
-    session[:user_id]= @user.id   #sign in the user who just signed up  
+    session[:user_id]= @user.id   #sign in the user who just signed up
     flash[:success] = "Welcome to the Alpha Blog, #{@user.username}!"
     redirect_to user_path(@user) #redirect the new user to his own page
 
@@ -44,6 +45,20 @@ class UsersController < ApplicationController
   @users = User.paginate(page: params[:page], per_page: 5)
 end
 
+
+def destroy   #delete user method
+
+@user = User.find(params[:id])
+
+@user.destroy
+
+flash[:danger] = "User and all articles created by user have been deleted"
+
+redirect_to users_path
+
+end
+
+
 def set_user
 
   @user = User.find(params[:id])   #find user based on id
@@ -52,9 +67,21 @@ end
 
 def require_same_user
 
-if current_user != @user  #if the user is not logged in and it is not the current user
+if current_user != @user and !current_user.admin?  #if the user is not logged in and it is not the current user
 
 flash[:danger] = "You can only edit your own account"
+
+redirect_to root_path
+
+end
+
+end
+
+def require_admin
+
+if logged_in? and !current_user.admin?
+
+flash[:danger] = "Only admin users can perform that action"
 
 redirect_to root_path
 
